0bb0b3de9e81f6b3e0aefc1a43e59429092a4fff
waranyoo3 <633230013@Webmail.npru.ac.th>
2023-09-17 10:50:34 +0700

    new

--- streamlit01.py
+++ streamlit01.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
+
 st.title('waranyoo')
+col1,col2 = st.columns(2)
+with col1: 
+    st.image("./pic/DSCF0584.jpg")
+with col2:
+    st.image("./pic/DSCF0584.jpg")
+        
 st.header("วรัญญู จุ๊กกรู้ 5555")
 st.subheader('pee')
 st.image ("./pic/css3logo.jpg")
