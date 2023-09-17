dc2c9d3be9d3f1ce50ade12d7ae82d412e318c96
waranyoo3 <633230013@Webmail.npru.ac.th>
2023-09-17 11:20:18 +0700

    new

--- streamlit01.py
+++ streamlit01.py
@@ -16,4 +16,15 @@ st.image ("./pic/css3logo.jpg")
 dt=pd.read_csv('data/iris.csv')
 st.write(dt.head(10))
 
-st.button("showchart")
+dt1 = dt['petal.length'].sum()
+dt2 = dt['petal.width'].sum()
+dt3 = dt['sepal.length'].sum()
+dt4 = dt['sepal.width'].sum()
+
+dx = [dt1, dt2, dt3, dt4]
+dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
+if st.button("showchart"):
+    dt.boxplot()
+
+else:
+    st.button("Don't show chart")
